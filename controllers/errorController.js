a51169242adb95ad1e6722434b34b15074c0d096
km-saifullah <kmsaifullah16@gmail.com>
2024-07-17 10:55:45 +0600

    handle production error and development error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,37 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  })
+}
+
+const sendErrorProduction = (err, res) => {
+  // operational, truested error: send  message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    })
+    // programming or other unknown error
+  } else {
+    // log error
+    console.error('Error:', err)
+    // send a generic message
+    res
+      .status(500)
+      .json({ status: 'error', message: 'Something went very wrong' })
+  }
+}
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  })
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res)
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProduction(err, res)
+  }
 }
