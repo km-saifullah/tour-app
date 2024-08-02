78c0d615f3e88285d0932d230f1437def1c67d83
km-saifullah <kmsaifullah16@gmail.com>
2024-08-02 21:50:14 +0600

    send email with reset password token

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const catchAsync = require('../utils/catchAsync')
 const User = require('./../models/userModel')
 const AppError = require('./../utils/appError')
+const sendEmail = require('./../utils/email')
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -114,6 +115,30 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false })
 
   // 3. send it to user's email
+  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/reset-password/${resetToken}`
+
+  const message = `Forgot your password? submit a PATCH request with your new password and passwordConfirm to: ${resetURL}\nIf you didn't forget your password, please ignore this email`
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'your password reset token (valid only for 10 min)',
+      message: message,
+    })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email',
+    })
+  } catch (error) {
+    user.passwordResetToken = undefined
+    user.passwordResetExpires = undefined
+    await user.save({ validateBeforeSave: false })
+    return next(
+      new AppError('There is an error to sending email. Try again later'),
+      500
+    )
+  }
 })
 
 exports.resetPassword = (req, res, next) => {}
---
+++ utils/email.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer')
+
+const sendEmail = async (options) => {
+  // 1. create a transporter
+  let transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  })
+
+  // 2. define the email options
+  const mailOptions = {
+    from: 'Khaled Saifullah <monmoygsc@gmail.com>',
+    to: options.email,
+    sub: options.subject,
+    text: options.message,
+  }
+
+  // 3. send the email with nodemailer
+  await transporter.sendMail(mailOptions)
+}
+
+module.exports = sendEmail
