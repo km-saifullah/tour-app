e16ff347e348d975fa99c662ea13a28e71370978
km-saifullah <kmsaifullah16@gmail.com>
2024-08-04 07:37:09 +0600

    reset password done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
 const catchAsync = require('../utils/catchAsync')
 const User = require('./../models/userModel')
 const AppError = require('./../utils/appError')
@@ -141,4 +142,34 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 })
 
-exports.resetPassword = (req, res, next) => {}
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1. get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex')
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  })
+
+  // 2. If token has not expired and there is user set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400))
+  }
+  user.password = req.body.password
+  user.passwordConfirm = req.body.passwordConfirm
+  user.passwordResetToken = undefined
+  user.passwordResetExpires = undefined
+  await user.save()
+
+  // 3. Update changed password at property for the user
+
+  // 4. Log the user in, send jwt
+  const token = signToken(user._id)
+  res.status(200).json({
+    status: 'success',
+    token,
+  })
+})
--- utils/email.js
+++ utils/email.js
@@ -3,8 +3,7 @@ const nodemailer = require('nodemailer')
 const sendEmail = async (options) => {
   // 1. create a transporter
   let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    service:"gmail",
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
